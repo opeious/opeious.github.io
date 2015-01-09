1b9dbfe02e25fec3fee5d4058a51b88440f5c2aa
Tapan Ranjan - RNR <tapanranjan@gmail.com>
2015-01-09 16:19:58 +0530

    maybeeee?
    ?

--- rnr/scripts/main.js
+++ rnr/scripts/main.js
@@ -19,17 +19,19 @@ var init = function() {
 				img7 = new Image();
                 
 				img0.src = "../img/mainbg.jpg";
-				img1.src = "../img/mainbg.jpg";
-				img2.src = "../img/mainbg.jpg";
-				img3.src = "../img/mainbg.jpg";
-				img4.src = "../img/mainbg.jpg";
-				img5.src = "../img/mainbg.jpg";
-				img6.src = "../img/mainbg.jpg";
-				img7.src = "../img/mainbg.jpg";
+				img1.src = "../img/mainbg1.jpg";
+				img2.src = "../img/mainbg2.jpg";
+				img3.src = "../img/mainbg3.jpg";
+				img4.src = "../img/mainbg4.jpg";
+				img5.src = "../img/mainbg5.jpg";
+				img6.src = "../img/mainbg6.jpg";
+				img7.src = "../img/mainbg7.jpg";
 			}
 
 		//--><!]]>
     document.getElementById("blankBG").style.opacity = 0;
+    document.getElementById("blankBG").style.display = "none";
+    
   var i =0;
   var images = ['../img/mainbg1.jpg','../img/mainbg2.jpg','../img/mainbg3.jpg','../img/mainbg4.jpg','../img/mainbg5.jpg','../img/mainbg6.jpg','../img/mainbg7.jpg','../img/mainbg.jpg'];
   var image = $('#bgs');
