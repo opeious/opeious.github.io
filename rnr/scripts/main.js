33667d0b644fafd413102f827f789c52b9305bd5
Tapan Ranjan - RNR <tapanranjan@gmail.com>
2015-01-09 16:12:39 +0530

    i just met you and this is crazy, so preload maybe

--- rnr/scripts/main.js
+++ rnr/scripts/main.js
@@ -3,8 +3,33 @@ var maxImg = 6;
 
 var init = function() {
     
-    document.getElementById("blankBG").style.opacity = 0;
     
+    
+    <!--//--><![CDATA[//><!--
+
+			if (document.images) {
+                
+				img0 = new Image();
+				img1 = new Image();
+				img2 = new Image();
+				img3 = new Image();
+				img4 = new Image();
+				img5 = new Image();
+				img6 = new Image();
+				img7 = new Image();
+                
+				img0.src = "../img/mainbg.jpg";
+				img1.src = "../img/mainbg.jpg";
+				img2.src = "../img/mainbg.jpg";
+				img3.src = "../img/mainbg.jpg";
+				img4.src = "../img/mainbg.jpg";
+				img5.src = "../img/mainbg.jpg";
+				img6.src = "../img/mainbg.jpg";
+				img7.src = "../img/mainbg.jpg";
+			}
+
+		//--><!]]>
+    document.getElementById("blankBG").style.opacity = 0;
   var i =0;
   var images = ['../img/mainbg1.jpg','../img/mainbg2.jpg','../img/mainbg3.jpg','../img/mainbg4.jpg','../img/mainbg5.jpg','../img/mainbg6.jpg','../img/mainbg7.jpg','../img/mainbg.jpg'];
   var image = $('#bgs');
