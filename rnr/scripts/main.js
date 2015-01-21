165b9316a77011ea82992222c55fb5b9a867849a
Tapan Ranjan - RNR <tapanranjan@gmail.com>
2015-01-21 22:07:32 +0530

    testing
    -

--- rnr/scripts/main.js
+++ rnr/scripts/main.js
@@ -1,4 +1,57 @@
-function preloader() {
+var flipper=1;
+
+function flip(){
+    if(flipper==1)
+    {
+        document.getElementById("flipped1").style.display = "none";
+        document.getElementById("flipped2").style.display = "block";
+        
+        flipper=0;
+    }
+    else{
+        
+     
+        document.getElementById("flipped2").style.display = "none";
+        document.getElementById("flipped1").style.display = "block";
+        
+        
+        
+        
+        
+        flipper=1;
+    }
+}
+
+function preloader(){
+    
+     moveProgressBar();
+    // on browser resize...
+    $(window).resize(function() {
+        moveProgressBar();
+    });
+
+    // SIGNATURE PROGRESS
+    function moveProgressBar() {
+        var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
+        var getProgressWrapWidth = $('.progress-wrap').width();
+        var progressTotal = getPercent * getProgressWrapWidth;
+        var animationLength = 2500;
+        
+        // on page load, animate percentage bar to data percentage length
+        // .stop() used to prevent animation queueing
+        $('.progress-bar').stop().animate({
+            left: progressTotal
+        }, animationLength);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
   
     
     <!--//--><![CDATA[//><!--
@@ -43,6 +96,10 @@ function addLoadEvent(func) {
 
 addLoadEvent(preloader);
 
+var im=0; 
+var image;
+var images = ['../img/mainbg1.jpg','../img/mainbg2.jpg','../img/mainbg3.jpg','../img/mainbg4.jpg','../img/mainbg5.jpg','../img/mainbg6.jpg','../img/mainbg7.jpg','../img/mainbg.jpg'];
+
 
 
 var init = function() {
@@ -51,24 +108,21 @@ var init = function() {
     document.getElementById("blankBG").style.opacity = 0;
     document.getElementById("blankBG").style.display = "none";
     
-  var i =0;
-  var images = ['../img/mainbg1.jpg','../img/mainbg2.jpg','../img/mainbg3.jpg','../img/mainbg4.jpg','../img/mainbg5.jpg','../img/mainbg6.jpg','../img/mainbg7.jpg','../img/mainbg.jpg'];
-  var image = $('#bgs');
-
+  image = $('#bgs');
   image.css('background-image', 'url("../img/mainbg.jpg")')
   
   setInterval(function(){  
 
    image.fadeOut(500, function () {
 
-   image.css('background-image', 'url(' + images [i++] +')');
-       
+   image.css('background-image', 'url(' + images [im++] +')');
+   if(im >= images.length)
+    im = 0;      
    image.fadeIn(500);
 
    });
 
-   if(i == images.length)
-    i = 0;
+
   }, 8000);           
  
 
@@ -76,8 +130,37 @@ var init = function() {
 
 };
 
+function rightImg()
+{
+   image.fadeOut(500, function () {
+
+   image.css('background-image', 'url(' + images [im] +')');
+   im++;
+       
+   if(im >= images.length)
+    im = 1;
+   image.fadeIn(500);
+
+   });
+
+    
+}
+
+function leftImg()
+{
+   image.fadeOut(500, function () {
+
+   image.css('background-image', 'url(' + images [im] +')');
+       im--;
+          if(im <= 0)
+    im = images.length-1;
+   image.fadeIn(500);
+
+   });
 
 
+    
+}
 
 
 
@@ -173,3 +256,19 @@ var init = function() {
 
 })();
 
+
+  $(document).ready(function () {
+
+        // you want to enable the pointer events only on click;
+
+        $('#map_canvas1').addClass('scrolloff'); // set the pointer events to none on doc ready
+        $('#canvas1').on('click', function () {
+            $('#map_canvas1').removeClass('scrolloff'); // set the pointer events true on click
+        });
+
+        // you want to disable pointer events when the mouse leave the canvas area;
+
+        $("#map_canvas1").mouseleave(function () {
+            $('#map_canvas1').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
+        });
+    });
