9ed1a5d9931851149de657f741f6864ab1ec0870
opeious <tapanranjan@gmail.com>
2015-06-18 03:37:18 +0530

    update
    june2015

---
+++ js/sections.js
@@ -0,0 +1,9 @@
+		$(document).ready(function() {
+			$('#fullpage').fullpage({
+				sectionsColor: ['#243445', '#222d3a', '#2B3540', '#30353B', '#ccddff'],
+				anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+				menu: '#menu',
+				scrollingSpeed: 1000
+			});
+
+		});
